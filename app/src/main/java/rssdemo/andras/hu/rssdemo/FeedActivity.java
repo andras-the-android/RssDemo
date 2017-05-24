41679e0792377c0f59a5ca40abccf2e07f529fdb
Andras Nemeth <guni82hu@gmail.com>
2017-05-24 21:38:19 +0200

    basic layout

--- app/src/main/java/rssdemo/andras/hu/rssdemo/FeedActivity.java
+++ app/src/main/java/rssdemo/andras/hu/rssdemo/FeedActivity.java
@@ -1,13 +1,63 @@
 package rssdemo.andras.hu.rssdemo;
 
+import android.content.res.Configuration;
+import android.databinding.DataBindingUtil;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.MenuItem;
+
+import rssdemo.andras.hu.rssdemo.databinding.ActivityFeedBinding;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private ActivityFeedBinding binding;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_feed);
+        setSupportActionBar(binding.toolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerToggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.app_name, R.string.app_name);
+        binding.drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        binding.recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
