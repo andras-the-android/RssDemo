985fbaa1a7b55a4692cc77932362ed701cb76e51
Andras Nemeth <guni82hu@gmail.com>
2017-05-24 20:29:40 +0200

    initial commit

---
+++ app/src/main/java/rssdemo/andras/hu/rssdemo/FeedActivity.java
@@ -0,0 +1,13 @@
+package rssdemo.andras.hu.rssdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
